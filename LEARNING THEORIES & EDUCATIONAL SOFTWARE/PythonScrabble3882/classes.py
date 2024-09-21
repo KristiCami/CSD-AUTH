2ce98225da07b42ff1da3d81d3227857732bbc44
Kristi Cami <92514755+KristiCami@users.noreply.github.com>
2024-09-21 13:06:16 +0300

    Add files via upload

---
+++ LEARNING THEORIES & EDUCATIONAL SOFTWARE/PythonScrabble3882/classes.py
@@ -0,0 +1,410 @@
+import json
+import random
+import sys
+from itertools import permutations
+
+LETTER_VALUES = {
+    "Α": 1,
+    "Β": 8,
+    "Γ": 4,
+    "Δ": 4,
+    "Ε": 1,
+    "Ζ": 10,
+    "Η": 1,
+    "Θ": 10,
+    "Ι": 1,
+    "Κ": 2,
+    "Λ": 3,
+    "Μ": 3,
+    "Ν": 1,
+    "Ξ": 10,
+    "Ο": 1,
+    "Π": 2,
+    "Ρ": 2,
+    "Σ": 1,
+    "Τ": 1,
+    "Υ": 2,
+    "Φ": 8,
+    "Χ": 8,
+    "Ψ": 10,
+    "Ω": 3,
+}
+
+
+class SakClass:
+    LETTER_QUANTITY = {
+        "Α": 12,
+        "Β": 1,
+        "Γ": 2,
+        "Δ": 2,
+        "Ε": 8,
+        "Ζ": 1,
+        "Η": 7,
+        "Θ": 1,
+        "Ι": 8,
+        "Κ": 4,
+        "Λ": 3,
+        "Μ": 3,
+        "Ν": 6,
+        "Ξ": 1,
+        "Ο": 9,
+        "Π": 4,
+        "Ρ": 5,
+        "Σ": 7,
+        "Τ": 8,
+        "Υ": 4,
+        "Φ": 1,
+        "Χ": 1,
+        "Ψ": 1,
+        "Ω": 3,
+    }
+
+    def __init__(self, number_of_letters):
+        self.numberOfLetters = number_of_letters
+
+    def getLetters(self, n):
+        """
+        Returns n random letters from the SakClass, subtracting the selected letters from the total pool.
+        """
+        letters = []
+        for i in range(n):
+            if sum(self.LETTER_QUANTITY.values()) == 0:
+                print("SakClass is empty!")
+                return letters
+            letter = random.choice(list(self.LETTER_QUANTITY.keys()))
+            while self.LETTER_QUANTITY[letter] == 0:
+                letter = random.choice(list(self.LETTER_QUANTITY.keys()))
+            self.LETTER_QUANTITY[letter] -= 1
+            letters.append(letter)
+        self.numberOfLetters -= n
+        return letters
+
+    def putBackLetters(self, letters):
+        """Puts back in the sack the letters """
+        for letter in letters:
+            if letter in self.LETTER_QUANTITY:
+                self.LETTER_QUANTITY[letter] += 1
+            else:
+                print('wrong letter')
+        self.numberOfLetters += len(letters)
+
+    def randomizeSak(self):
+        """Returns an array of 7 randomized letters"""
+        return self.getLetters(7)
+
+    def printSakStatus(self):
+        """Prints information about the current status of the Sak """
+        print("Letters left:", self.LETTER_QUANTITY, "\n Letters remaining: ", self.numberOfLetters)
+
+
+class Player:
+    current_letters = []
+    score = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def printCurrentLetters(self):
+        """Prints player's current letters"""
+        print(self.current_letters)
+
+    def printCurrentLettersFormatted(self):
+        """prints current letters and their values in a formatted way"""
+        print("Available letters and their values: ", end="")
+        for letter in self.current_letters:
+            print(f"{letter},{LETTER_VALUES[letter]} \t", end="")
+        print()  # add a new line  at the end
+
+    def getLetterValues(self, letters):
+        """Receives an array of letters and returns a dictionary with the value of each letter """
+        letter_values = {}
+        for letter in letters:
+            if letter in LETTER_VALUES:
+                letter_values[letter] = LETTER_VALUES[letter]
+        return letter_values
+
+
+class Human(Player):
+
+    def __init__(self, name):
+        self.totalWordsHuman = 0
+        self.totalPassesHuman = 0
+        super().__init__(name)
+
+
+class Computer(Player):
+
+    def __init__(self, name):
+        self.totalWordsComputer = 0
+        self.totalPassesComputer = 0
+        super().__init__(name)
+
+    def play(self, game, algorithmOption):
+        """Computer plays based on the algorithmOption parameter"""
+        if algorithmOption == 1:
+            # Play min letters algorithm
+            return self.minLetters(game)
+        elif algorithmOption == 2:
+            # Play max letters algorithm
+            return self.maxLetters(game)
+        elif algorithmOption == 3:
+            # Play smart algorithm
+            return self.smart(game)
+
+    def minLetters(self, game):
+        """Implementation of the MIN LETTERS algorithm"""
+        print("--- Min letters algorithm ---")
+        # Get all permutations of the available letters, starting with length 2
+        perms = []
+        for i in range(2, len(self.current_letters) + 1):
+            for p in permutations(self.current_letters, i):
+                perms.append(''.join(p))
+
+        # Find the first valid word in the dictionary (assuming uppercase)
+        with open('greek7.txt', 'r', encoding='utf-8') as f:
+            for line in f:
+                word = line.strip()
+                if word in perms:
+                    print("Word played by computer:", word, " - ", game.calculateWordPoints(word), " points")
+                    game.wordsformed += 1
+                    return word
+        print("Computer could not find valid word. Computer is passing its turn!")
+        return 'p'
+
+    def maxLetters(self, game):
+        """Implementation of the MAX LETTERS algorithm"""
+        print("--- Max letters algorithm ---")
+        for i in range(len(self.current_letters), 1, -1):
+            perms = [''.join(p) for p in permutations(self.current_letters, i)]
+            with open('greek7.txt', 'r', encoding='utf-8') as f:
+                for line in f:
+                    word = line.strip()
+                    if word in perms:
+                        print("Word played by computer: ", word, " - ", game.calculateWordPoints(word), " points")
+                        game.wordsformed += 1
+                        return word
+        print("Computer could not find valid word. Computer is passing its turn!")
+        return 'p'
+
+    def smart(self, game):
+        """Implemetation of the SMART algorithm"""
+        print("--- Smart algorithm ---")
+
+        # Get all permutations of the available letters, starting with length 2
+        perms = []
+        for i in range(2, len(self.current_letters) + 1):
+            for p in permutations(self.current_letters, i):
+                perms.append(''.join(p))
+
+        # Find the valid words with the highest point value
+        max_word = ''
+        max_points = 0
+        with open('greek7.txt', 'r', encoding='utf-8') as f:
+            for line in f:
+                word = line.strip()
+                if word in perms:
+                    points = game.calculateWordPoints(word)
+                    if points > max_points:
+                        max_word = word
+                        max_points = points
+
+        if max_word:
+            print("Word played by computer: ", max_word, " - ", game.calculateWordPoints(max_word), " points")
+            game.wordsformed += 1
+            return max_word
+        else:
+            print("Computer could not find valid word. Computer is passing its turn!")
+            return 'p'
+
+
+class Game:
+    wordsformed = 0
+
+    def __init__(self, gameName):
+        self.gameName = gameName
+
+    def displayStartingScreen(self):
+        """Displays the starting screen for the Scrabble game."""
+        print("******** WELCOME TO SCRABBLE ********")
+        print("-------------------------")
+        print("1: Stats")
+        print("2: Settings")
+        print("3: Play")
+        print("4: Quit")
+        print("-------------------------")
+
+    def printScore(self, pHuman, pComputer):
+        """Prints current score"""
+        print(f"Player: {pHuman.name} - Score: {pHuman.score}")
+        print(f"Player: {pComputer.name} - Score: {pComputer.score}")
+
+    def readWord(self, active_player):
+        """Reads word and returns """
+        while True:
+            word = input("Enter your word or press 'p' to pass your turn or press 'q' to quit: ")
+            if word == 'q':
+                return word
+            elif word == 'p':
+                # increase human player passes by one
+                active_player.totalPassesHuman += 1
+                return word
+            # Check if word cant be formed with the available letters and if it exists in the greek7.txt
+            elif self.validateWord(active_player, word) and self.wordExists(word):
+                # Increase words formed by one
+                active_player.totalWordsHuman += 1
+                # Print word points message
+                print(f"Word played by {active_player.name}: {word} - {self.calculateWordPoints(word)} points")
+                return word
+            else:
+                print("Invalid word. Please try again.")
+
+    def validateWord(self, player, word):
+        """Validates if the given word can be formed from the current_letters array"""
+        available_letters = player.current_letters.copy()  # create a copy of current_letters
+        for letter in word:
+            if letter not in available_letters:
+                print(f"---Word '{word}' can't be formed with the available letters---")
+                return False
+            available_letters.remove(letter)
+        return True
+
+    def wordExists(self, word):
+        """Checks if the word is a valid word in greek7.txt"""
+        with open('greek7.txt', 'r', encoding='utf-8') as f:
+            for line in f:
+                if word == line.strip():
+                    self.wordsformed += 1
+                    return True
+        print(f"---Word '{word}' does not exist---")
+        return False
+
+    def calculateWordPoints(self, word):
+        """Calculates the point value of a word based on the global LETTER_VALUES dictionary"""
+        word_points = 0
+        for letter in word:
+            letter_points = LETTER_VALUES.get(letter, 99999)
+            word_points += letter_points
+        return word_points
+
+    def printGameInfo(self, player, sak):
+        """Prints player and game information"""
+        print("******************************************************")
+        print("Playing: ", player.name)
+        print("Score: ", player.score)
+        print("Available letters: ", player.current_letters)
+        player.printCurrentLettersFormatted()
+        print("Letters remaining in the sak: ", sak.numberOfLetters)
+        print("******************************************************")
+
+    def printGameInfoAfterMove(self, player, sak):
+        """Prints information right after a player or the pc has played its turn"""
+        print("------------------------------------------------------")
+        print("Player:", player.name, "- Score:", player.score, "- Letters remaining in the sak:", sak.numberOfLetters)
+        print("Available letters: ", player.current_letters)
+        print("------------------------------------------------------")
+
+    def setup(self, pHuman, pComputer, sak):
+        """Initializes saks"""
+        # Initialize sak for human player
+        pHuman.current_letters = sak.randomizeSak()
+        # Initialize sak for pc player
+        pComputer.current_letters = sak.randomizeSak()
+
+    def end(self, pHuman, pComputer):
+        """Ends game after printing game result information and saving it to a file """
+        if pHuman.score > pComputer.score:
+            winner = pHuman.name
+        elif pHuman.score < pComputer.score:
+            winner = pComputer.name
+        else:
+            winner = 'Game is a tie'
+        print("Goodbye!")
+        print(f"Player: {pHuman.name} - Score: {pHuman.score} ")
+        print(f"Player: {pComputer.name} - Score: {pComputer.score}")
+        print(f"Winner: {winner}")
+        # Save in a file game info like the winner, scores, number of words formed, number of passes etc
+        self.saveGameStats(pHuman, pComputer, winner)
+        sys.exit()
+
+    def updateSak(self, sak, active_player, word):
+        """Updates sak after a play, performing the necessary actions"""
+        for letter in word:
+            if letter in active_player.current_letters:
+                active_player.current_letters.remove(letter)
+        new_letters = sak.getLetters(7 - len(active_player.current_letters))
+        active_player.current_letters.extend(new_letters)
+
+    def passTurn(self, sak, active_player):
+        """Passes turn, performing the necessary actions"""
+        sak.putBackLetters(active_player.current_letters)
+        active_player.current_letters = sak.getLetters(len(active_player.current_letters))
+        print("Turn passed. New available letters: ", active_player.current_letters)
+
+    def changePlayer(self, active_player, pHuman, pComputer):
+        """Changes active player"""
+        if active_player == pHuman:
+            active_player = pComputer
+        else:
+            active_player = pHuman
+        return active_player
+
+    def saveGameStats(self, pHuman, pComputer, winner):
+        """Saves game statistics to a file called lastgamestats.json"""
+        stats = {
+            "winner": winner,
+            "human_score": pHuman.score,
+            "computer_score": pComputer.score,
+            "human_name": pHuman.name,
+            "computer_name": pComputer.name,
+            "words_formed": self.wordsformed,
+            "words_formed_human": pHuman.totalWordsHuman,
+            "words_formed_computer": pComputer.totalWordsComputer,
+            "passes_computer": pComputer.totalPassesComputer,
+            "passes_human": pHuman.totalPassesHuman
+        }
+        with open("lastgamestats.json", "w") as f:
+            json.dump(stats, f)
+
+    def loadLastGameStats(self):
+        """Loads stats from file lastgamestats.json and prints them"""
+        try:
+            with open("lastgamestats.json", "r") as f:
+                stats = json.load(f)
+
+            # extract the stats from the JSON object
+            winner = stats["winner"]
+            human_score = stats["human_score"]
+            human_name = stats["human_name"]
+            computer_score = stats["computer_score"]
+            computer_name = stats["computer_name"]
+            words_formed = stats["words_formed"]
+            words_formed_human = stats["words_formed_human"]
+            words_formed_computer = stats["words_formed_computer"]
+            passes_human = stats["passes_human"]
+            passes_computer = stats["passes_computer"]
+
+            # print the stats to the console
+            print("-------------------------------------------------")
+            print("Last game stats:")
+            print(f"Winner: {winner}")
+            print(f"Player: {human_name} - Score {human_score}")
+            print(f"Player: {computer_name} - Score {computer_score}")
+            print(f"Total words formed: {words_formed}")
+            print(f"Words formed by {human_name}: {words_formed_human}")
+            print(f"Words formed by Computer: {words_formed_computer}")
+            print(f"Total passes by {human_name}: {passes_human}")
+            print(f"Total passes by Computer: {passes_computer}")
+            print("-------------------------------------------------")
+        except FileNotFoundError:
+            print("-------------------------------------------------")
+            print("No previous game stats found!")
+            print("-------------------------------------------------")
+
+    def displaySettingScreen(self):
+        """Displays settings screen"""
+        print("-----------------------------------------")
+        print("Please select the algorithm that you wish the Computer to play with. Choose 1, 2 or 3")
+        print("1 - MIN LETTERS")
+        print("2 - MAX LETTERS")
+        print("3 - SMART")
+        print("-----------------------------------------")
---
+++ LEARNING THEORIES & EDUCATIONAL SOFTWARE/PythonScrabble3882/main3882.py
@@ -0,0 +1,144 @@
+import sys
+
+from classes import SakClass, Game, Human, Computer
+
+
+def guidelines():
+    """
+    Provides guidelines for playing Scrabble.
+
+    1) Classes implemented: SakClass, Game, Player, Human, Computer
+
+    2) Inheritance:
+    Base class: Player
+    Derived classes: Human, Computer
+
+    3) Extension methods in derived classes:
+    Class Computer: play(), minLetters(), maxLetters(), smart()
+    Class Human: -
+
+    4) Neither decorators nor operator overloading was needed in the Scrabble implementation.
+
+    5) Dictionary data structure was used to handle the available words.
+
+    6) MIN-MAX-SMART algorithms were implemented for the computer to play.
+
+    7) Gameflow:
+     User enters his name. A screen with the available options will appear. Choose 1, 2, 3 or 4
+     If you choose 1 last game stats will appear.
+     If you choose 2 you will be able to choose the algorithm to play the computer with
+     If you choose 4 you will quit the game
+     If you choose 3 you will start playing the game. By default, the human plays first. Each time
+     you are informed with your current letters and their values, letters remaining in the sak,
+     your current score. After you type in a valid word you are informed about its points, your updated
+     score and your updated letters. This happens when it's time for the computer to play.
+
+
+     IMPORTANT NOTES:
+     1) By default the computer plays with the 'MIN LETTERS' algorithm. In case you want
+     a different algorithm, when starting the game go to settings and change it.
+
+     2) To be able to view the stats you will have to have played at least 1 game.
+
+     3) When it is your turn to play a word,
+     type it in UPPERCASE GREEK only. To pass your turn use 'p' and to quit the game use 'q'
+
+    4) After typing a word or passing your turn, take some time to look through the printed information.
+
+    Usage: help(guidelines) to view this documentation.
+    """
+
+
+# Scrabble in greek edition has 102 letters (excluding wildcards)
+# Create SakClass object with 102 letters
+sak = SakClass(102)
+
+# Create game object
+game = Game("Scrabble")
+
+# Read player's name
+nameHuman = input("Welcome to Scrabble!\nEnter your name: ")
+
+# Create human object
+pHuman = Human(nameHuman)
+
+# Create computer object
+pComputer = Computer("PC")
+
+# Game setup
+game.setup(pHuman, pComputer, sak)
+
+# Initialize gameOver flag
+isGameOver = False
+
+# Set active player
+active_player = pHuman
+
+# Set default algorithm to MIN LETTERS
+algorithmOption = 1
+
+word_dictionary = {}
+# Load the words from the greek7.txt to a dictionary
+with open("greek7.txt", "r", encoding='utf-8') as file:
+    for line in file:
+        word = line.strip()
+        word_dictionary[word] = True
+
+while True:
+    game.displayStartingScreen()
+    # Get player choice (1-4) and validity test for user input
+    playerChoice = input("Select option: ")
+    while playerChoice not in ["1", "2", "3", "4"]:
+        print("Invalid input. Please select 1, 2, 3 or 4")
+        playerChoice = input("Select option: ")
+    playerChoice = int(playerChoice)
+
+    if playerChoice == 1:
+        print("Stats")
+        game.loadLastGameStats()
+    elif playerChoice == 2:
+        print("Settings")
+        game.displaySettingScreen()
+        algorithmOption = input("Select algorithm: ")
+        while algorithmOption not in ["1", "2", "3"]:
+            print("Invalid input. Please select 1, 2 or 3")
+            algorithmOption = input("Select algorithm: ")
+        algorithmOption = int(algorithmOption)
+    elif playerChoice == 3:
+        print("Play")
+        while not isGameOver and sak.numberOfLetters > 0:
+            # Print information about the game
+            game.printGameInfo(active_player, sak)
+            # Read word and validate it
+            if active_player == pHuman:
+                word = game.readWord(active_player)
+            elif active_player == pComputer:
+                word = pComputer.play(game, algorithmOption)
+                # Increase computer counters
+                if word == 'p':
+                    pComputer.totalPassesComputer += 1
+                else:
+                    pComputer.totalWordsComputer += 1
+            if word == 'q':
+                game.end(pHuman, pComputer)
+            # Check if word exists
+            elif word == 'p':
+                game.passTurn(sak, active_player)
+            else:
+                # calculate word points and add it to the score of the active player
+                word_points = game.calculateWordPoints(word)
+                # Update active player's score
+                active_player.score += word_points
+                # Update sak
+                game.updateSak(sak, active_player, word)
+                # Print game info
+                game.printGameInfoAfterMove(active_player, sak)
+            # Change active player
+            active_player = game.changePlayer(active_player, pHuman, pComputer)
+            # Check if Sak is empty
+            if sak.numberOfLetters <= 0:
+                print("Sak has emptied! Game over!")
+                game.end(pHuman, pComputer)
+    elif playerChoice == 4:
+        print("Quitting the game...")
+        sys.exit()
